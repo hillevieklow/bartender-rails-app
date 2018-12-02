7f52dd18ec8084280c328a0bec6b6c0d26f9c54c
hillevieklow <hillevieklow@gmail.com>
2018-12-02 13:05:48 -0500

    initializes ingredientcontroller methods

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,25 @@
 class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def show
+    find_recipe_ingredient_and_ingredient
+    # binding.pry
+  end
+
+
+  private
+
+  def find_recipe_ingredient_and_ingredient
+    @ingredient = Ingredient.find_by(params[:id])
+    @recipe_ingredient = RecipeIngredient.find_by(params[:id])
+  end
+
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
+
 end
