9c7d7b04cee0e01721250634abc14920e04305e3
hillevieklow <hillevieklow@gmail.com>
2018-12-02 13:06:55 -0500

    adds more methods outside create, changes params to now also permit ingredient_id

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,27 +7,66 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = current_user.recipes.build
+    @ingredients = Ingredient.all
+  end
+
+  def show
+    find_recipe
+    @recipe_ingredient = RecipeIngredient.find(params[:id])
+    # @ingredient = Ingredient.find(params[:id])
+  end
+
+  def upvote
+    find_recipe
+    @recipe.update(:upvotes => @recipe.upvotes+1)
+    redirect_to recipe_path(@recipe)
   end
 
   def create
-    binding.pry
+    # binding.pry
     @recipe = current_user.recipes.build(recipe_params)
 
     if @recipe.valid?
       @recipe.save
+      redirect_to @recipe
     else
+      @ingredients = Ingredient.all
       render :new
     end
   end
 
+  def edit
+    find_recipe
+    @ingredients = Ingredient.all
+  end
+
+  def update
+    find_recipe
+    @recipe.update(recipe_params)
+    if @recipe.valid?
+      @recipe.save # not sure
+      redirect_to recipe_path(@recipe)
+    else
+      @ingredients = Ingredients.all
+      render :edit
+    end
+  end
+
+  def destroy
+    find_recipe
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :description, :direction, recipe_ingredients_attributes: [ :quantity, ingredient_attributes: [ :name ] ])
+    params.require(:recipe).permit(:name, :description, :directions, recipe_ingredients_attributes: [ :quantity, :ingredient_id, ingredient_attributes: [ :name ] ])
   end
 
   def find_recipe
-    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find_by(params[:id])
     if !@recipe
       redirect_to user_path(@current_user)
     end
