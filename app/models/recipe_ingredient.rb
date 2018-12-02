a44dad843a8e73e5f117e8c0abb87a4904210e57
hillevieklow <hillevieklow@gmail.com>
2018-12-02 13:08:53 -0500

    adds accepts_nested_attributes_for, and rejeccts if formbox is blank

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,5 +1,10 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
-  accepts_nested_attributes_for :ingredient
+  validates :quantity, presence: true
+
+  accepts_nested_attributes_for :ingredient, allow_destroy: true,
+ reject_if: proc { |attributes| attributes['name'].blank? }
+
+
 end
