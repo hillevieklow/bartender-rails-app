47357e9946fcee517a311785750ed0c175fb88ff
hillevieklow <hillevieklow@gmail.com>
2018-12-02 13:14:57 -0500

    db/migrate/20181116154931_create_recipe_ingredients.rb

--- db/migrate/20181116154931_create_recipe_ingredients.rb
+++ db/migrate/20181116154931_create_recipe_ingredients.rb
@@ -4,7 +4,7 @@ class CreateRecipeIngredients < ActiveRecord::Migration[5.2]
       t.string :name
       t.integer :recipe_id
       t.integer :ingredient_id
-      t.integer :quantity
+      t.string :quantity
       t.timestamps
     end
   end
