733ee5907f1976bbcf84646089c0bdf2633b3856
hillevieklow <hillevieklow@gmail.com>
2018-12-02 13:08:07 -0500

    implements an activerecord scope method that finds recipes that includes the chosen ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,12 @@
 class Ingredient < ApplicationRecord
-  has_many :recipe_ingedients
-  has_many :recipes, :through => :recipe_ingedients
+  has_many :recipe_ingredients
+  has_many :recipes, :through => :recipe_ingredients
+  validates :name, presence: true, uniqueness: true
+  before_save { |ingredient| ingredient.name = ingredient.name.titleize }
+
+
+  def self.recipes
+    includes(recipe: :recipe_ingredients).where(recipe_ingredients: {name: self})
+  end
+
 end
