6a7e6dc92eb7aee9cfe98a429046b27ad1c9d50b
hillevieklow <hillevieklow@gmail.com>
2018-12-03 13:26:11 -0500

    allows for omniauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,23 @@
 class SessionsController < ApplicationController
   def create
-  @user = User.find_by(email: params[:email].downcase)
-  if !@user
-    @user = User.new(email: params[:email])
-    render :welcome
-  elsif @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    redirect_to user_path(@user)
-  else
-    render :welcome
+    redirect_to user_path(@current_user.id) if logged_in?
+    if auth_hash = request.env["omniauth.auth"]
+      @user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id]=@user.id
+      redirect_to user_path(@user)
+    else
+      @user = User.find_by(email: params[:email].downcase)
+      if !@user
+        @user = User.new(email: params[:email])
+        render :welcome
+      elsif @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        render :welcome
+      end
+    end
   end
-end
 
   def destroy
     session.delete :user_id
