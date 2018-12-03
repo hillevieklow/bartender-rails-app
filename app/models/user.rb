55510cd2b0ac51325713b66e4abb769dd90876d3
hillevieklow <hillevieklow@gmail.com>
2018-12-03 13:26:37 -0500

    allows for omniauth login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   has_secure_password
   before_save { |user| user.email = user.email.downcase }
 
+  def self.find_or_create_by_omniauth(auth_hash)
+    email = auth_hash[:info][:email]
+    user = find_by(:email => email)
+    if !user
+      user=User.create(:email => email, :password => SecureRandom.hex,
+        :name => auth_hash[:info][:name])
+    end
+    user
+  end
+
 end
