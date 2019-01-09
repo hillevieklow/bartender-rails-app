eab2dda2ebe23dd75f71537e336918f232355426
hillevieklow <hillevieklow@gmail.com>
2019-01-09 20:40:27 +0100

    allow nested route recipe/:id/recipe_ingredient/new

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,16 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   get "/auth/facebook/callback" => "sessions#create"
-  get 'users/:id/recipes' => 'users#index'
 
-  resources :recipes
+
+  resources :recipes do
+    resources :recipe_ingredients
+  end
+  
   resources :users do
     resources :recipes
   end
+
 
   resources :ingredients, only: [:show]
 
