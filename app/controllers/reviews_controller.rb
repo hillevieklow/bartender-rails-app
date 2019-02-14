ea4659df210fbd2da5594df3f5b8fabd47a77305
hillevieklow <hillevieklow@gmail.com>
2019-02-14 12:52:53 -0500

    delete index function, add create function and a private review_params function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,22 @@
 class ReviewsController < ApplicationController
   before_action :set_recipe
 
-  def index
-    @reviews = @recipe.reviews
-    # render :layout => false
-    render :json => @reviews
+  def create
+    @review = current_user.reviews.build(review_params)
+    @review.recipe = @recipe
+    if @review.save
+      render json: @review, status: 201
+    else
+      render json: {errors: @review.errors.full_messages}, status: 400
+    end
   end
 
   private
   def set_recipe
-    @recipe = Recipe.find(params[:recipe_id]) # loads recipe into @recipe variable
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :title, :user_id, :recipe_id)
   end
 end
