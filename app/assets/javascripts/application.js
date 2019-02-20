d2fd93da9588b6a89802bfeb3d344baa5e45aa1b
hillevieklow <hillevieklow@gmail.com>
2019-02-20 13:41:36 -0500

    implement changes suggested by Brad - replacing concatenation with back-ticks and cleaning up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,13 @@
 
 $(function(){
 
-  // RECIPE index
+  /////////////////////////////////////////////
+  // ------- JS FOR RECIPE INDEX PAGE -------//
+  /////////////////////////////////////////////
 
-  var recipeArray = [];
-  var id = parseInt($(".js-next").attr("data-id"));
+
+  let recipeArray = [];
+  let id = parseInt($(".js-next").attr("data-id"));
 
   if ($("#recipesInfo").length) {
     loadAllRecipes();
@@ -35,7 +38,7 @@ $(function(){
        recipeArray = data;
        $.each(
          recipeArray, function(index, recipe){
-           var recipeData = "<h4><a href='/recipes/" + recipe.id + "'>"
+           let recipeData = "<h4><a href='/recipes/" + recipe.id + "'>"
             + recipe.name + "</a>, " + recipe.upvotes + " upvotes</h4>"
             + "<div id='description-" + recipe.id + "'>"
             + recipe.description.substring(0, 120) + "..."
@@ -43,74 +46,71 @@ $(function(){
            $('#recipesInfo').append(recipeData);
          }
        )
-       recipeArray.sort()
      });
   }
 
   $("#recipesInfo").on('click', '.js-more', function(e){
     e.preventDefault();
-    var id = this.dataset.id;
+    let id = this.dataset.id;
     $.get("/recipes/" + id + ".json", function(data){
       $("#description-" + id).html(data.description)
     });
   });
 
-// RECIPE show
 
+  ////////////////////////////////////////////
+  // ------- JS FOR RECIPE SHOW PAGE -------//
+  ////////////////////////////////////////////
+
+
+  // Loads all the data from the DOM into JS
   function loadRecipe(data){
-      history.pushState({}, "", "/recipes/" + data.id)
-      var recipeReviewPath = '/recipes/' + data.id + '/reviews/';
-      $("#new_review").attr('action', recipeReviewPath);
+      history.pushState({}, "", `/recipes/${data.id}`)
+
       $(".recipeName").text(data["name"]);
       $(".recipeUpvotes").text(data["upvotes"]);
-      // // $("#author").empty();
-      // var author = new Author(data["user"])
-      // author.renderDisplay
       $(".recipeUser").text(data["user"]["name"]);
       $(".recipeDescription").text(data["description"]);
       $(".recipeDirections").text(data["directions"]);
-      $("#ri-original").empty();
-      $("#ri-list").empty();
-      data["recipe_ingredients"].forEach(function(element){
-        var ri = new RecipeIngredient(element);
-        ri.renderDisplay();
-      })
       $(".js-next").attr("data-id", data["id"]);
       $(".js-previous").attr("data-id", data["id"]);
+
+      $("#ri-original").empty();
+      $("#ri-list").empty();
+      data["recipe_ingredients"].forEach(function(recipeIngredient){
+        let newRecipeIngredient = new RecipeIngredient(recipeIngredient);
+        let recipeIngredientHtml = newRecipeIngredient.renderDisplay();
+        $("#ri-list").append(recipeIngredientHtml);
+      })
+
+      let recipeReviewPath = `/recipes/${data.id}/reviews/`;
+      $("#new_review").attr('action', recipeReviewPath);
       $("#submitted-reviews").empty();
-      data["review_list"].forEach(function(element){
-        var review = new Review(element);
-        review.renderDisplay();
+      data["review_list"].forEach(function(review){
+        let newReview = new Review(review);
+        let reviewHtml = newReview.renderDisplay();
+        $("#submitted-reviews").append(reviewHtml);
       });
+
   }
 
-  // function Author(data){
-  //   this.author = data.name;
-  // }
-  //
-  // Author.prototype.renderDisplay = function(){
-  //   var authorHtml = "";
-  //   authorHtml += "this.author"
-  //   $("#author").append(authorHtml)
-  // }
-
-  function RecipeIngredient(data){
-    this.id = data.id;
-    this.name = data.name;
-    this.quantity = data.quantity;
+  // ES6 RecipeIngredient class (model object) initialized using constructor method
+  class RecipeIngredient{
+    constructor(data){
+      this.id = data.id;
+      this.name = data.name;
+      this.quantity = data.quantity;
+    }
   }
 
+  // JS model object custom function for rendering display of RecipeIngredient
   RecipeIngredient.prototype.renderDisplay = function(){
-    var html = "" ;
-    html += "<h4>" + "<a href='/ingredients/" + this.id + "'>" + this.name + "</a>"+ ", " + this.quantity+"</h4>"
-    // "<div class=\"ri\"><h4><a href=\"ingredients/" + this.id + ">" + this.name + "</a> heheh</h4></div>";
-      $("#ri-list").append(html);
+    return (`<h4><a href='/ingredients/${this.id}'> ${this.name}</a>, ${this.quantity}</h4>`)
   }
 
-// NEXT button
-
+  // NEXT button
   $(".js-next").on("click", function(e){
-    var id = $(".js-next").attr("data-id")
+    let id = $(".js-next").attr("data-id")
     $.get("/recipes/" + id + "/next", function(data){
       console.log(data)
       loadRecipe(data);
@@ -118,49 +118,64 @@ $(function(){
     e.preventDefault();
   });
 
-// PREVIOUS button
+  // PREVIOUS button
   $(".js-previous").on("click", function(e){
-    var id = $(".js-previous").attr("data-id")
+    let id = $(".js-previous").attr("data-id")
     $.get("/recipes/" + id + "/previous", function(data){
       console.log(data)
       loadRecipe(data);
     });
     e.preventDefault();
-
   });
+
+  // ES6 Review class (model object) initialized using constructor method
+  class Review{
+    constructor(data){
+    this.id = data.id;
+    this.title = data.title;
+    this.content = data.content;
+    this.user = data.user;
+    }
+  }
+
+  // JS model object custom function for rendering display of Review
+  Review.prototype.renderDisplay = function(){
+    return (`<div class="well well-white" id="review-${this.id}"><h4>${this.title}</h4><p>${this.content}</p><strong>- ${this.user.name}</strong></div>`)
+  }
+
+  // Form submitted throguh AJAX
+  $(function() {
+    $("form#new_review").on("submit", function(e){
+      e.preventDefault();
+      let $form = $(this);
+      let action = $form.attr("action");
+      let params = $form.serialize();
+      $.ajax({
+        url: action,
+        data: params,
+        dataType: "json",
+        method: "POST"
+      })
+      .success(function(json){
+        $(".reviewBox").val("");
+        let newReview = new Review(json);
+        let reviewHtml = newReview.renderDisplay();
+        $("#submitted-reviews").append(reviewHtml);
+      })
+    });
+  })
 
 });
 
 
-function Review(data){
-  this.id = data.id;
-  this.title = data.title;
-  this.content = data.content;
-  this.user = data.user;
-}
 
-Review.prototype.renderDisplay = function(){
-  var html = "" ;
-  html += "<div class=\'well well-white\' id=\'review-\' + review.id + '\'>" + '<h4>' + this.title + '</h4>' + '<p>' + this.content + '</p>' + '<strong>' + '- ' + "<a href='/users/" + this.user.id + "/recipes'>" + this.user.name + "</a>" + '</strong>' + "</div>";
-    $("#submitted-reviews").append(html);
-}
 
-$(function() {
-  $("form#new_review").on("submit", function(e){
-    e.preventDefault();
-    var $form = $(this);
-    var action = $form.attr("action");
-    var params = $form.serialize();
-    $.ajax({
-      url: action,
-      data: params,
-      dataType: "json",
-      method: "POST"
-    })
-    .success(function(json){
-      $(".reviewBox").val("");
-      var review = new Review(json);
-      review.renderDisplay();
-    })
-  })
-})
+
+
+
+
+// 1. Loop through array
+// 2. Create a class instance
+// 3. Call custom function on the new instance
+// 4. Append result (html you got from cusotm function) to the DOM
+// 5.
