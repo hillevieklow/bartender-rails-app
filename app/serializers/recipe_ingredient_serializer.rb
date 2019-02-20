2202279876dc3a605e470b78a2e666d85a8a5a72
hillevieklow <hillevieklow@gmail.com>
2019-02-20 13:42:14 -0500

    add name function that returns the ingredient name

--- app/serializers/recipe_ingredient_serializer.rb
+++ app/serializers/recipe_ingredient_serializer.rb
@@ -2,4 +2,8 @@ class RecipeIngredientSerializer < ActiveModel::Serializer
   attributes :id, :quantity
   belongs_to :ingredient
   belongs_to :recipe
+
+  def name
+    return ingredient.name
+  end
 end
